bbd737009de463bd5ac694125f627d756f3d686d
Pablo Orviz <orviz@ifca.unican.es>
2017-11-22 08:09:21 +0000

    rpm command to install slurm packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,22 +10,10 @@ class slurm inherits slurm::params {
 
 class slurm::install {
     if $::osfamily == "RedHat" {
-        $pkgs = {
-            "slurm-plugins" => { source   => "https://depot.galaxyproject.org/yum/el/7/x86_64/slurm-plugins-17.02.6-1.el7.centos.x86_64.rpm",
-                                 name     => "slurm-plugins",
-                                 ensure   => installed,
-                                 provider => rpm },
-            "slurm"         => { source   => "https://depot.galaxyproject.org/yum/el/7/x86_64/slurm-17.02.6-1.el7.centos.x86_64.rpm",
-                                 name     => "slurm",
-                                 ensure   => installed,
-                                 provider => rpm },
-            "slurm-munge"   => { source   => "https://depot.galaxyproject.org/yum/el/7/x86_64/slurm-munge-17.02.6-1.el7.centos.x86_64.rpm",
-                                 name     => "slurm-munge", 
-                                 ensure   => installed,
-                                 provider => rpm },
-
+        exec {
+            "Install slurm RPMs":
+                command => "/usr/bin/rpm -ivh https://depot.galaxyproject.org/yum/el/7/x86_64/slurm-plugins-17.02.6-1.el7.centos.x86_64.rpm https://depot.galaxyproject.org/yum/el/7/x86_64/slurm-17.02.6-1.el7.centos.x86_64.rpm https://depot.galaxyproject.org/yum/el/7/x86_64/slurm-munge-17.02.6-1.el7.centos.x86_64.rpm",
         }
-        create_resources(package, $pkgs)
     }
 }
 
